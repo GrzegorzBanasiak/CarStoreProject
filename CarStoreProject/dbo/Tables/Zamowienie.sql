4cc769da3c8695ae6553c475dc363cbf6e4936a7
Dzynq <grzegorzbanasiak95@gmail.com>
2019-02-14 11:20:51 +0100

    Dodanie kolumny cena do tabeli Zamowienie, Dodanie widoku Ilość Zamówień Marki.

--- CarStoreProject/dbo/Tables/Zamowienie.sql
+++ CarStoreProject/dbo/Tables/Zamowienie.sql
@@ -1,17 +1,20 @@
 ﻿CREATE TABLE [dbo].[Zamowienie] (
-    [ID]            INT  IDENTITY (1, 1) NOT NULL,
-    [ModelID]       INT  DEFAULT ((-1)) NOT NULL,
-    [CennikID]      INT  DEFAULT ((-1)) NOT NULL,
-    [PracownikID]   INT  DEFAULT ((-1)) NOT NULL,
-    [KlientID]      INT  DEFAULT ((-1)) NOT NULL,
-    [Zrealizowane]  BIT  NOT NULL,
-    [Oplacone]      BIT  NOT NULL,
-    [DatZamowienia] DATE NOT NULL,
-    [DatOdbioru]    DATE NOT NULL,
+    [ID]            INT   IDENTITY (1, 1) NOT NULL,
+    [ModelID]       INT   DEFAULT ((-1)) NOT NULL,
+    [CennikID]      INT   DEFAULT ((-1)) NOT NULL,
+    [PracownikID]   INT   DEFAULT ((-1)) NOT NULL,
+    [KlientID]      INT   DEFAULT ((-1)) NOT NULL,
+    [Zrealizowane]  BIT   NOT NULL,
+    [Oplacone]      BIT   NOT NULL,
+    [DatZamowienia] DATE  NOT NULL,
+    [DatOdbioru]    DATE  NOT NULL,
+    [Cena]          MONEY NOT NULL,
     PRIMARY KEY CLUSTERED ([ID] ASC),
     FOREIGN KEY ([CennikID]) REFERENCES [dbo].[Cennik] ([ID]) ON DELETE SET DEFAULT,
     FOREIGN KEY ([KlientID]) REFERENCES [dbo].[Klient] ([ID]) ON DELETE SET DEFAULT,
     FOREIGN KEY ([ModelID]) REFERENCES [dbo].[Marka] ([ID]) ON DELETE SET DEFAULT,
     FOREIGN KEY ([PracownikID]) REFERENCES [dbo].[Pracownik] ([ID]) ON DELETE SET DEFAULT
 );
+
+
 
---
+++ CarStoreProject/rapOp/Views/IloscZamowienMarki.sql
@@ -0,0 +1,14 @@
+﻿/*
+CREATE SCHEMA [rapOp] 
+	AUTHORIZATION [dbo];
+
+ALTER TABLE [dbo].[Zamowienie] Add Cena money NOT NULL;
+
+*/
+
+CREATE VIEW [rapOp].[IloscZamowienMarki] as 
+	SELECT m.Nazwa, count(z.ID) as IloscZamowienMarki, sum(z.Cena) as Kwota
+	FROM dbo.Marka as m with(nolock) 
+	LEFT JOIN dbo.Model as model on m.ID = model.MarkaID
+	LEFT JOIN dbo.Zamowienie as z on z.ModelID = model.ID
+	GROUP BY m.Nazwa
