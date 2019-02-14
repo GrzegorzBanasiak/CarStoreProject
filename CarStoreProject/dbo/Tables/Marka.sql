b0f5edfea2a4034751dd7f3d2847bc0fcb7d3931
Dzynq <grzegorzbanasiak95@gmail.com>
2019-02-14 10:20:52 +0100

    Test Commit, Dodanie tabeli Marka Samochodu.

---
+++ CarStoreProject/dbo/Tables/Marka.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Marka] (
+    [ID]    INT           IDENTITY (1, 1) NOT NULL,
+    [Nazwa] VARCHAR (200) NOT NULL,
+    [Kraj]  VARCHAR (200) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    UNIQUE NONCLUSTERED ([Nazwa] ASC)
+);
+
