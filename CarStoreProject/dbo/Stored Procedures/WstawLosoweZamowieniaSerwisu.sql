c34ead437b4429bc244a06cd67916b652bea8bda
Dzynq <grzegorzbanasiak95@gmail.com>
2019-02-14 13:39:36 +0100

    Procedura dodawania losowych danych do tabeli Serwis.Zamowienia oraz procedura dodajaca dane do bazy danych.

---
+++ CarStoreProject/dbo/Stored Procedures/WstawLosoweZamowieniaSerwisu.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE WstawLosoweZamowieniaSerwisu @ileZamowien as INT as
+BEGIN
+	
+	DECLARE @obrotPetli as INT
+	SET @obrotPetli = 0 
+	
+	DECLARE @pracownikID as INT
+	DECLARE @klientID as INT
+	DECLARE @uslugaID as INT
+		
+	WHILE @obrotPetli < @ileZamowien
+	BEGIN
+
+	-- Przydzielanie losowych wartosci
+	SET @pracownikID = (SELECT TOP 1 ID FROM [dbo].[Pracownik] ORDER BY NEWID())
+	SET @klientID = (SELECT TOP 1 ID FROM [dbo].[Klient] ORDER BY NEWID())
+	SET @uslugaID = (SELECT TOP 1 ID FROM [Serwis].[Usluga] ORDER BY NEWID())
+
+	-- Przypisanie wartosci do tabeli
+	INSERT INTO [Serwis].[Zamowienie] (PracownikID, KlientID, UslugaID) VALUES
+	(@pracownikID, @klientID, @uslugaID)
+
+	SET @obrotPetli = @obrotPetli + 1 
+
+	END
+END
