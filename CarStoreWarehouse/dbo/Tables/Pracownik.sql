346ecb59ade6c3fdf5d43280b4e11b44a0b14a69
Dzynq <grzegorzbanasiak95@gmail.com>
2019-02-14 16:10:25 +0100

    Dodanie tabeli pracownik.

---
+++ CarStoreWarehouse/dbo/Tables/Pracownik.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Pracownik] (
+    [ID]             INT           IDENTITY (1, 1) NOT NULL,
+    [Imie]           VARCHAR (200) NOT NULL,
+    [Nazwisko]       VARCHAR (200) NOT NULL,
+    [PESEL]          BIGINT        NOT NULL,
+    [NrKonta]        BIGINT        NOT NULL,
+    [Zespol]         VARCHAR (200) NOT NULL,
+    [Stanowisko]     VARCHAR (200) NOT NULL,
+    [NaStanowiskuOD] DATE          NOT NULL,
+    [NaStanowiskuDO] DATE          NOT NULL,
+    [Wynagrodzenie]  MONEY         NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    UNIQUE NONCLUSTERED ([PESEL] ASC)
+);
+
